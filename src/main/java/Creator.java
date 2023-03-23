bfa5fb906ab909dde6b0e8f88a7ced8541ef9d59
ildar <zariil@yandex.ru>
2023-03-23 08:01:16 +0500

    added #1319. Number of Operations to Make Network Connected

--- src/main/java/Creator.java
+++ src/main/java/Creator.java
@@ -3,7 +3,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Creator {
-    private static final String fileName = "2492. Minimum Score of a Path Between Two Cities";
+    private static final String fileName = "1319. Number of Operations to Make Network Connected";
 
     public static void main(String[] args) throws IOException {
         String packageName = getPackageName();
